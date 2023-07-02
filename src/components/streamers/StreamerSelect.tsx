b11006320f94e436f4d7d6af859f8138569bd357
Kamil Gajewski <kwaq102@gmail.com>
2023-07-02 22:21:18 +0200

    added style for select streamer page

--- src/components/streamers/StreamerSelect.tsx
+++ src/components/streamers/StreamerSelect.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 const StreamerSelect = () => {
 	return (
-		<>
-			<p>Wybierz streamera</p>
-			<Link to="../">Wróć do listy</Link>
-		</>
+		<div className="streamerSelect">
+			<p className="streamerSelect__info">You have to choose some streamer</p>
+			<Link to="/streamers" className="streamerSelect__link">
+				<button className="streamerSelect__button btn">Streamer list</button>
+			</Link>
+		</div>
 	);
 };
 
