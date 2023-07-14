180159a362d2e7332897e6a42a2aa12f342f92b8
Kamil Gajewski <kwaq102@gmail.com>
2023-07-14 09:16:12 +0200

    removed comments

--- src/utils/navigationList.ts
+++
@@ -1,7 +0,0 @@
-export const navList = [
-    { navName: "Home", path: "/", end: true },
-    { navName: "Show All", path: "/streamers" },
-    // { navName: "Streamer info", path: `/streamers/${streamerId}` },
-];
-
-// TODO spróbować tu dać niezależnie id
